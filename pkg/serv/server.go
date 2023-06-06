40e6134634ebdcc93f2e240266291f4411f99034
kirill <kirill.petrov.2001@list.ru>
2023-06-06 17:45:54 +0300

    Подправил логи

--- pkg/serv/server.go
+++ pkg/serv/server.go
@@ -22,13 +22,13 @@ func (ApiServ) GetHardwareValue(cont context.Context, req *pr.HardwareRequest) (
 
 	dbPool, err := pgxpool.New(context.Background(), os.Getenv("DB"))
 	if err != nil {
-		log.Fatalf("Я еблан забыл переделать логи: %v\n", err)
+		log.Fatalf("Логи 1: %v\n", err)
 		return nil, errors.New("Ошибка подключения к БД")
 	}
 
 	rows, err := dbPool.Query(context.Background(), "select param_name, current_value from public.unit u join public.params p on p.param_id = u.param_id where u.hardware_id = $1;", req.HarwareId)
 	if err != nil {
-		log.Fatalf("Я снова еблан забыл переделать логи %v\n", err)
+		log.Fatalf("Логи 2 %v\n", err)
 		return nil, errors.New("Ошибка выполнения SQL запроса")
 	}
 
@@ -37,13 +37,13 @@ func (ApiServ) GetHardwareValue(cont context.Context, req *pr.HardwareRequest) (
 		var r pr.HardwareParams
 		err := rows.Scan(&r.ParamName, &r.ParamValue)
 		if err != nil {
-			log.Fatal("Кто еблан забывший переделать логи?")
+			log.Fatal("Логи 3")
 			return nil, errors.New("Ошибка чтения запроса")
 		}
 		ret = append(ret, &r)
 
 	}
-	//Добавить генерацию Message id и узнать действительно ли она нужна
+
 	return &pr.HardwareResponse{MessageId: uuid.NewV4().String(), Params: ret}, nil
 }
 
@@ -52,25 +52,24 @@ func (ApiServ) UpdateParamValue(cont context.Context, req *pr.UpdateRequest) (*p
 
 	dbPool, err := pgxpool.New(context.Background(), os.Getenv("DB"))
 	if err != nil {
-		log.Fatalf("Я еблан забыл переделать логи: %v\n", err)
+		log.Fatalf("Логи 4 %v\n", err)
 		return nil, errors.New("Ошибка подключения к БД")
 	}
-	//Скорее всего можно сделать лучше, но пока и так сойдёт
+
 	for _, val := range req.Params {
 		_, err := dbPool.Exec(context.Background(), "UPDATE public.params SET p.current_value=$1 from public.params p join public.unit u on  p.param_id = u.param_id  join public.hardware h on h.hardware_id = u.hardware_id  WHERE h.hardware_id = $2 and p.param_id = $3;", val.ParamValue, req.HardwareId, val.ParamId)
 		if err != nil {
-			log.Fatal("Я снова еблан забыл переделать логи")
+			log.Fatal("Логи 5")
 			return nil, errors.New("Ошибка выполнения SQL запроса")
 		}
 	}
 
 	//Далее идёт передача на сериализатор, которого пока нет
 
-	//Добавить генерацию Message id и узнать действительно ли она нужна
 	return &pr.UpdateResponse{MessageId: uuid.NewV4().String(), ErrorCode: "OK"}, nil
 }
 
-// Нихуя не работает
+// TODO...
 func (ApiServ) Registration(ctx context.Context, req *pr.RegistrationRequest) (*pr.RegistrationResponse, error) {
 	return &pr.RegistrationResponse{}, nil
 }
