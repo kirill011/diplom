406fabb23750d26e57dbfb701ec304eff87fc25e
kirill <kirill.petrov.2001@list.ru>
2023-06-06 18:33:25 +0300

    Проверка существующих функций

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -21,4 +21,10 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(res.Params)
+	par := make([]*api.UpdateParams, 1, 1)
+	par[0] = &api.UpdateParams{ParamId: 1, ParamValue: 1}
+	res2, err := client.UpdateParamValue(context.Background(), &api.UpdateRequest{HardwareId: 1, Token: "", Params: []*api.UpdateParams{
+		&api.UpdateParams{ParamId: 1, ParamValue: 1},
+	}})
+	fmt.Println(res2)
 }
