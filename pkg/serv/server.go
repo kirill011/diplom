09a472bffaafde805b664f369fc6bfae7b0aa4c2
kirill <kirill.petrov.2001@list.ru>
2023-06-16 12:59:04 +0300

    Провожу тесты

--- pkg/serv/server.go
+++ pkg/serv/server.go
@@ -41,7 +41,7 @@ func (ApiServ) GetHardwareValue(cont context.Context, req *pr.HardwareRequest) (
 		var r pr.HardwareParams
 		err := rows.Scan(&r.ParamName, &r.ParamValue)
 		if err != nil {
-			log.Fatal("Логи 3")
+			log.Fatalf("Логи 3 %v\n", err)
 			return nil, errors.New("Ошибка чтения запроса")
 		}
 		ret = append(ret, &r)
@@ -54,26 +54,29 @@ func (ApiServ) GetHardwareValue(cont context.Context, req *pr.HardwareRequest) (
 // Обновление базы и пересылка на сериализатор
 func (ApiServ) UpdateParamValue(cont context.Context, req *pr.UpdateRequest) (*pr.UpdateResponse, error) {
 
-	dbPool, err := pgxpool.New(context.Background(), os.Getenv("DB"))
+	/*dbPool, err := pgxpool.New(context.Background(), os.Getenv("DB"))
 	if err != nil {
 		log.Fatalf("Логи 4 %v\n", err)
 		return nil, errors.New("Ошибка подключения к БД")
 	}
-
+	//Добавить проверку
 	for _, val := range req.Params {
-		_, err := dbPool.Exec(context.Background(), "UPDATE public.params SET p.current_value=$1 from public.params p join public.unit u on  p.param_id = u.param_id  join public.hardware h on h.hardware_id = u.hardware_id  WHERE h.hardware_id = $2 and p.param_id = $3;", val.ParamValue, req.HardwareId, val.ParamId)
+		_, err := dbPool.Exec(context.Background(), "UPDATE public.params SET current_value=$1 from public.params p join public.unit u on  p.param_id = u.param_id  join public.hardware h on h.hardware_id = u.hardware_id  WHERE h.hardware_id = $2 and p.param_id = $3;", val.ParamValue, req.HardwareId, val.ParamId)
 		if err != nil {
-			log.Fatal("Логи 5")
+			log.Fatalf("Логи 5 %v\n", err)
 			return nil, errors.New("Ошибка выполнения SQL запроса")
 		}
-	}
+	}*/
 
 	conn, err := grpc.Dial("77.222.42.182:50051", grpc.WithInsecure())
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Логи 6 %v\n", err)
 	}
 	client := send.NewUnaryClient(conn)
 	res, err := client.SendToClient(context.Background(), &send.Message{})
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(res)
 	return &pr.UpdateResponse{MessageId: uuid.NewV4().String(), ErrorCode: "OK"}, nil
 }
@@ -82,12 +85,12 @@ func (ApiServ) UpdateParamValue(cont context.Context, req *pr.UpdateRequest) (*p
 func (ApiServ) Registration(ctx context.Context, req *pr.RegistrationRequest) (*pr.RegistrationResponse, error) {
 	dbPool, err := pgxpool.New(context.Background(), os.Getenv("DB"))
 	if err != nil {
-		log.Fatalf("Логи 4 %v\n", err)
+		log.Fatalf("Логи 7 %v\n", err)
 		return nil, errors.New("Ошибка подключения к БД")
 	}
 	_, err = dbPool.Exec(context.Background(), "INSERT INTO public.users(login, password, actual) VALUES($1, $2, true);", req.Login, req.Password)
 	if err != nil {
-		log.Fatal("Логи 5")
+		log.Fatal("Логи 8")
 		return nil, errors.New("Ошибка выполнения SQL запроса")
 	}
 	return &pr.RegistrationResponse{MessageId: uuid.NewV4().String(), ErrorCode: "OK"}, nil
