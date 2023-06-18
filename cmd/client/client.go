34a1a84b68e0cd34d670f16d86789b96685bda7c
kirill <kirill.petrov.2001@list.ru>
2023-06-18 21:19:51 +0300

    d

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -30,15 +30,9 @@ func main() {
 
 	req := &api.HardwareIdRequest{Token: "WWFzaGE6TGF2YQ=="}
 	res2, err := client.GetHardwareId(metadata.AppendToOutgoingContext(context.Background(), "token", "WWFzaGE6TGF2YQ=="), req)
-	if err != nil {
-		errorLog.Printf("Client: %v\n", err)
-	}
 	fmt.Println(res2)
 
 	req2 := &api.HardwareRequest{HarwareId: 1, Token: "WWFzaGE6TGF2YQ=="}
 	res3, err := client.GetHardwareValue(metadata.AppendToOutgoingContext(context.Background(), "token", "WWFzaGE6TGF2YQ=="), req2)
-	if err != nil {
-		errorLog.Printf("Client: %v\n", err)
-	}
 	fmt.Println(res3)
 }
