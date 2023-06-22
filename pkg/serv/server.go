f0f46d8996b39e8d5b90542691ec9d40848c27ab
kirill <kirill.petrov.2001@list.ru>
2023-06-23 00:28:35 +0300

    00

--- pkg/serv/server.go
+++ pkg/serv/server.go
@@ -120,7 +120,7 @@ func (ApiServ) UpdateParamValue(ctx context.Context, req *pr.UpdateRequest) (*pr
 			ret, errSend = client.SendToClient(context.Background(), &send.Message{Host: host, HardId: req.HardwareId, ComandId: val.ParamId, Value: val.ParamValue, MessageId: messageId})
 			if errSend != nil {
 				errorLog.Printf("UpdateParamValue: %v MessageId : %v\n", errSend, messageId)
-
+				ret.ErrorCode = "Not OK"
 			}
 		}
 
